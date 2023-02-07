a3643aab06cecf4e05a077f3dc616b8d91b65269
Bryan Mills <bryan.mills@cloudzero.com>
2023-02-07 13:51:15 -0500

    intitial commit

---
+++ .storybook/main.js
@@ -0,0 +1,21 @@
+import path from 'node:path'
+/** @type { import('@storybook/react-vite').StorybookConfig } */
+const config = {
+  "stories": [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    path.dirname(require.resolve(path.join('@storybook/addon-links', 'package.json'))),
+    path.dirname(require.resolve(path.join('@storybook/addon-essentials', 'package.json'))),
+    path.dirname(require.resolve(path.join('@storybook/addon-interactions', 'package.json'))),
+  ],
+  "framework": {
+    "name": path.dirname(require.resolve(path.join('@storybook/react-vite', 'package.json'))),
+    "options": {}
+  },
+  "docs": {
+    "autodocs": "tag"
+  }
+};
+export default config;
